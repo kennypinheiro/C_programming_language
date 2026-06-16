49e279c61904e47942e18e355b3b54fe7e48c8d2
mol3cul4RNA <kennypinheiro@users.noreply.github.com>
2026-06-15 22:28:37 -0300

    Add files via upload

--- pointer.c
+++ pointer.c
@@ -7,6 +7,8 @@ int main(void){
     char a[4]={'A', 'T', 'G', 'C'};
 
     pointer = a;
+    
+    /* Obs: char a = {'A', 'T', 'G', 'C'};  --> pointer = &a */
 
     for (int i =0; i < 4; i++){
         printf("%c\n", *(a+i));
@@ -23,7 +25,7 @@ int main(void){
 
     printf("Pointer -> %p\n", &pointer);
     printf("Pointer -> %p\n", pointer);
-    printf("Pointer -> %p\n", &a);
-    printf("Pointer -> %p\n", a);
+    printf("Variavel a -> %p\n", &a);
+    printf("Variavel a -> %p\n", a); //Mostra o endereço da primeira posição do vetor (se fosse uma vaiável iria mostrar o conteúdo armazenado em hexadecimal)
     return 0;
 }
